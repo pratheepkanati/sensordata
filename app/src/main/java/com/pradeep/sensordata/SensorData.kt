ad29360d113644fd29c15063ecd85a9a9398a2a0
Pratheep kanati <39478564+pratheepkanati@users.noreply.github.com>
2022-03-07 12:19:08 +0530

    minor changes

--- app/src/main/java/com/pradeep/sensordata/SensorData.kt
+++ app/src/main/java/com/pradeep/sensordata/SensorData.kt
@@ -1,7 +1,7 @@
 package com.pradeep.sensordata
 
 import android.app.Application
-
+//https://www.youtube.com/watch?v=9tTKa9jEgNs  e notation is applicable for sensor data if number is too long need to convert to decimal notion
 class SensorData: Application() {
     override fun onCreate() {
         super.onCreate()
